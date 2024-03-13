3b14e68267461c28ef1452bc12cba607e8540a66
Vasi-Cordea <150058199+Vasi-Cordea@users.noreply.github.com>
2024-03-13 22:29:07 +0200

    POM - Page Object Model created

--- CucumberJava/src/test/java/pages/loginPage.java
+++ CucumberJava/src/test/java/pages/loginPage.java
@@ -4,40 +4,45 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class loginPage {
-	
+
 	WebDriver driver;
-	
-	 public static By acceptaTot_ByXpath = By.xpath("//div[contains(@class,'QS5gu sy4vM') and normalize-space(text())='Acceptă tot' ]");
-	 public static By txt_username = By.id("username");
-	 public static By txt_password = By.id("password");
-	 public static By btn_submit=By.id("submit");
-	public static By btn_logOut =  By.xpath("//*[contains(text(), 'Log out')]");
-	 
+
+	By acceptaTot_ByXpath = By
+			.xpath("//div[contains(@class,'QS5gu sy4vM') and normalize-space(text())='Acceptă tot' ]");
+	By txt_username = By.id("username");
+	By txt_password = By.id("password");
+	By btn_submit = By.id("submit");
+	By btn_logOut = By.xpath("//*[contains(text(), 'Log out')]");
+
 	public loginPage(WebDriver driver) {
-		
-		driver =driver;
+
+		// driver = driver;
 	}
-	
-	 
-	 public void enterUsername (String username) {
-		 
-		 driver.findElement(txt_username).sendKeys(username);
-		 
-		  }
-	 
- public void enterPassword (String password) {
-		 
-		 driver.findElement(txt_password).sendKeys(password);
+
+	// create separate function for every action on the page
+	public void enterUsername(String username) {
+
+		driver.findElement(txt_username).sendKeys(username);
+
+	}
+
+	public void enterPassword(String password) {
+
+		driver.findElement(txt_password).sendKeys(password);
+	}
+
+	public void clickLogin() {
+
+		driver.findElement(btn_submit).click();
+
+	}
+
+	public void loginValidUser(String username, String password) {
+		driver.findElement(txt_username).sendKeys(username);
+		driver.findElement(txt_password).sendKeys(password);
+		driver.findElement(btn_submit).click();
+	}
+
 }
- public void clickLogin () {
-	 
-	 driver.findElement(btn_submit).click();
- 
-}
- public void loginValidUser(String username, String password) {
-	 driver.findElement(txt_username).sendKeys(username);
-	 driver.findElement(txt_password).sendKeys(password);
-	 driver.findElement(btn_submit).click();
- }
- 
- }
+
+//  CTRL+SHIFT+F - code auto formatting
